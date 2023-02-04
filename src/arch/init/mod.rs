111707cdaf054b518b8f2840e541dfe2819291b5
Lysander Mealy <lysander.mealy@gmail.com>
2023-02-04 18:16:37 -0500

    feat(arm): :zap: Remove `cfg-if`
    It wasn't really useful.

--- src/arch/init/mod.rs
+++ src/arch/init/mod.rs
@@ -1,7 +1,2 @@
-use cfg_if::cfg_if;
-
-cfg_if! {
-    if #[cfg(all(target_arch = "aarch64", feature = "linux"))] {
-        mod aarch64_linux;
-    }
-}
+#[cfg(all(target_arch = "aarch64", feature = "linux"))]
+mod aarch64_linux;
