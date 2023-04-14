e2c5f3218f904382c6b3cfbb3f297443fbea5388
Lysander Mealy <lysander.mealy@gmail.com>
2023-04-14 09:47:35 -0400

    feat(libmem): :sparkles: Rewrite `Slab` to wrap its allocator
    The `Slab` can now do its own locking on the allocator.

--- libs/mem/src/slab.rs
+++ libs/mem/src/slab.rs
@@ -1,28 +1,42 @@
+use core::cell::UnsafeCell;
+
 use alloc::{vec, vec::Vec};
+use system::sync::{Lock, Mutex};
 
 pub trait Alloc {
     type Item: Clone;
-    async fn alloc() -> Option<Self::Item>;
-    async fn free(item: Self::Item);
+    async fn alloc(&mut self) -> Option<Self::Item>;
+    async fn free(&mut self, item: Self::Item);
 }
 
 pub struct Slab<A: Alloc, const N: usize> {
-    slabs: Vec<heapless::Vec<A::Item, N>>,
+    slabs: UnsafeCell<Vec<heapless::Vec<A::Item, N>>>,
+    alloc: Mutex<A>,
 }
 
 unsafe impl<A: Alloc, const N: usize> Send for Slab<A, N> {}
 unsafe impl<A: Alloc, const N: usize> Sync for Slab<A, N> {}
 
 impl<A: Alloc, const N: usize> Slab<A, N> {
-    pub fn new() -> Self {
+    pub fn new(alloc: A) -> Self {
         Self {
-            slabs: vec![heapless::Vec::new(); system::cpus::CpuInfo::num_cpus()],
+            slabs: UnsafeCell::new(vec![
+                heapless::Vec::new();
+                system::cpus::CpuInfo::num_cpus()
+            ]),
+            alloc: Mutex::new(alloc),
         }
     }
 
-    pub fn alloc(&mut self) -> Option<A::Item> {
+    pub fn empty(&self) -> bool {
         let cpu_id = system::cpus::CpuInfo::cpu_id();
-        let slab = &mut self.slabs[cpu_id];
+        let slab = &self.get_slabs()[cpu_id];
+        slab.is_empty()
+    }
+
+    pub fn alloc(&self) -> Option<A::Item> {
+        let cpu_id = system::cpus::CpuInfo::cpu_id();
+        let slab = &mut self.get_slabs_mut()[cpu_id];
         if let Some(item) = slab.pop() {
             Some(item)
         } else {
@@ -30,24 +44,24 @@ impl<A: Alloc, const N: usize> Slab<A, N> {
         }
     }
 
-    pub async fn free(&mut self, item: A::Item) {
+    pub async fn free(&self, item: A::Item) {
         let cpu_id = system::cpus::CpuInfo::cpu_id();
-        let slab = &mut self.slabs[cpu_id];
+        let slab = &mut self.get_slabs_mut()[cpu_id];
         if let Err(item) = slab.push(item) {
-            A::free(item).await;
+            self.alloc.lock().await.free(item).await;
         }
     }
-    pub fn free_nolock(&mut self, item: A::Item) -> Result<(), A::Item> {
+    pub fn free_nolock(&self, item: A::Item) -> Result<(), A::Item> {
         let cpu_id = system::cpus::CpuInfo::cpu_id();
-        let slab = &mut self.slabs[cpu_id];
+        let slab = &mut self.get_slabs_mut()[cpu_id];
         slab.push(item)
     }
 
-    pub async fn restock(&mut self) -> bool {
+    pub async fn restock(&self) -> bool {
         let cpu_id = system::cpus::CpuInfo::cpu_id();
-        let slab = &mut self.slabs[cpu_id];
+        let slab = &mut self.get_slabs_mut()[cpu_id];
         for _ in 0..N - slab.len() {
-            if let Some(item) = A::alloc().await {
+            if let Some(item) = self.alloc.lock().await.alloc().await {
                 let _ = slab.push(item);
             } else {
                 return false;
@@ -56,7 +70,7 @@ impl<A: Alloc, const N: usize> Slab<A, N> {
         true
     }
 
-    pub async fn alloc_restocking(&mut self) -> Option<A::Item> {
+    pub async fn alloc_restocking(&self) -> Option<A::Item> {
         if let Some(item) = self.alloc() {
             Some(item)
         } else {
